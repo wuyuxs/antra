f2ce1fc61c189544df8b05a9faf9044ee81df84b
wuyuxs <wuyuxs@qq.com>
2024-08-13 12:34:44 -0400

    reset button works

--- practice-app/src/app/profile/profile.component.ts
+++ practice-app/src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,10 +6,38 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  counter:number = 0;
   profileForm = new FormGroup({
     userName: new FormControl(''),
     email: new FormControl(''),
     bio: new FormControl('')
   })
+  profile = {
+    userName: this.profileForm.value.userName,
+    email: this.profileForm.value.email,
+    bio: this.profileForm.value.bio
+  }
+  disableReset:boolean = true;
+  welcomeMsg:string = `Welcome!${this.profileForm.value.userName ? ' ' + this.profileForm.value.userName : ''}`;
+  ngOnInit(): void {
+    this.profileForm.get('userName')?.valueChanges.subscribe(value => {
+      this.welcomeMsg = `Welcome!${value ? ' ' + value : ''}`;
+    });
+    this.profileForm.valueChanges.subscribe(() => {
+      this.disableReset = this.isFormEmpty();
+    });
+  }
+
+  saveProfile() {
+    console.log(this.profileForm.value);
+    this.counter++;
+    this.welcomeMsg = `Welcome!${this.profileForm.value.userName ? ' ' + this.profileForm.value.userName : ''}`;
+  }
+  resetProfile() {
+    this.profileForm.reset();
+  }
+  isFormEmpty():boolean {
+    return !(this.profileForm.value.userName || this.profileForm.value.email || this.profileForm.value.bio);
+  }
 }
